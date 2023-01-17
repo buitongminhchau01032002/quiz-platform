3554a2131863072ff26c60b2d809f6adaf9804cd
Minh Chau <buitongminhchau01032002@gmail.com>
2023-01-17 11:18:23 +0700

    add data

--- src/data/quiz.js
+++ src/data/quiz.js
@@ -1,181 +1,68 @@
 const QUIZ = {
-    name: 'dfasdfasd',
+    name: 'Ôn tập Javascript cơ bản',
     questions: [
         {
             type: 'single-choose',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            answers: [
-                'piscing elit, sed do eiusmod tempor',
-                'consectetur adipiiusmod tempor',
-                'adipiscing elit, sed do eiusmod tempor',
-                'fasdf adipiscing elit, sed do eiusmod tempor',
-            ],
+            content: 'Viết code javascript bên trong thẻ html nào?',
+            answers: ['<scriping>', '<script>', '<javascript>', '<js>'],
+            correctAnswer: 1,
+            hint: 'Có thể viết javascript ngay trong html bằng 1 thẻ nào đó',
+            explanation: 'Thẻ <script> dùng để viết code Javascript',
+        },
+        {
+            type: 'single-choose',
+            content: 'Một hàm được truyền vào một hàm khác dưới dạng tham số được gọi là gì?',
+            answers: ['Callback', 'Expression function', 'Arrow function', 'Closure'],
             correctAnswer: 0,
-            hint: 'em ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun',
-            explanation:
-                'm dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et do',
+            hint: null,
+            explanation: 'Nó là callback',
         },
         {
             type: 'single-choose',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            answers: [
-                'consectetur adipiiusmod tempor',
-                'adipiscing elit, sed do eiusmod tempor',
-                'fasdf adipiscing elit, sed do eiusmod tempor',
-            ],
-            correctAnswer: 1,
-            hint: '',
+            content: 'Đâu là một trong các kiểu dữ liệu Reference',
+            answers: ['String', 'Null', 'Object', 'Number'],
+            correctAnswer: 2,
+            hint: 'Có 2 loại kiểu dữ liệu là Primtive và Reference',
             explanation:
-                'm dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et do',
+                'Kiểu Primtive bao gồm: Number, String, Boolean, Null, Undefined, Symbols. Kiểu Reference bao gồm: Object, Function, Array, và các kiểu khác của Object...',
         },
         {
             type: 'single-choose',
-            content:
-                'Lorem ipsum dolor sore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            content: 'IIFE trong javascript là gì',
             answers: [
-                'consectetur adipiiusmod tempor',
-                'consectetur adipiiusmod tempor',
-                'adipiscing elit, sed do eiusmod tempor',
-                'fasdf adipiscing elit, sed do eiusmod tempor',
+                'Một hàm được truyền vào hàm khác dưới dạng tham số',
+                'Một hàm dùng để khởi tạo một Object',
+                'Một hàm không có giá trị trả về',
+                'Một hàm được khởi tạo và thực thi ngay lập tức',
             ],
-            correctAnswer: 1,
-            hint: 'Hahhaha ahha a ahhdfa sdf hasdf as dfasd fasdfhahs fasd',
-            explanation:
-                'm dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et do',
+            correctAnswer: 3,
+            hint: 'IIFE là viết tắt của Immediately Invoked Function Expression',
+            explanation: null,
         },
         {
             type: 'single-choose',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            answers: [
-                'piscing elit, sed do eiusmod tempor',
-                'consectetur adipiiusmod tempor',
-                'adipiscing elit, sed do eiusmod tempor',
-                'fasdf adipiscing elit, sed do eiusmod tempor',
-            ],
+            content: 'Điền vào chỗ trống: Global variables can be made local (private) with ______.',
+            answers: ['Closure', 'Callback', 'Promise', 'Async Function'],
             correctAnswer: 0,
-            hint: 'em ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun',
-            explanation:
-                'm dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et do',
+            hint: 'Đây là một định nghĩa theo W3schools',
+            explanation: null,
         },
         {
             type: 'single-choose',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            answers: [
-                'consectetur adipiiusmod tempor',
-                'adipiscing elit, sed do eiusmod tempor',
-                'fasdf adipiscing elit, sed do eiusmod tempor',
-            ],
-            correctAnswer: 1,
-            hint: '',
-            explanation:
-                'm dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et do',
+            content: 'Đâu không phải là trạng thái của một Promise',
+            answers: ['Pending', 'Fulfilled', 'Errored', 'Rejected'],
+            correctAnswer: 2,
+            hint: 'Một Promise có 3 trạng thái',
+            explanation: '3 trạng thái của một Promise là pending, fulfilled và rejected',
         },
         {
             type: 'single-choose',
-            content:
-                'Lorem ipsum dolor sore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            answers: [
-                'consectetur adipiiusmod tempor',
-                'consectetur adipiiusmod tempor',
-                'adipiscing elit, sed do eiusmod tempor',
-                'fasdf adipiscing elit, sed do eiusmod tempor',
-            ],
-            correctAnswer: 1,
-            hint: '',
+            content: 'Callback truyền vào hàm setTimeout sẽ được đưa vào đâu?',
+            answers: ['Call Stack', 'Queue', 'Web API'],
+            correctAnswer: 2,
+            hint: 'setTimeout được cung cấp bởi trình duyệt',
             explanation:
-                'm dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et do',
-        },
-        {
-            type: 'single-choose',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            answers: [
-                'piscing elit, sed do eiusmod tempor',
-                'consectetur adipiiusmod tempor',
-                'adipiscing elit, sed do eiusmod tempor',
-                'fasdf adipiscing elit, sed do eiusmod tempor',
-            ],
-            correctAnswer: 0,
-            hint: '',
-            explanation:
-                'm dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et do',
-        },
-        {
-            type: 'single-choose',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            answers: [
-                'consectetur adipiiusmod tempor',
-                'adipiscing elit, sed do eiusmod tempor',
-                'fasdf adipiscing elit, sed do eiusmod tempor',
-            ],
-            correctAnswer: 1,
-            hint: '',
-            explanation:
-                'm dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et do',
-        },
-        {
-            type: 'single-choose',
-            content:
-                'Lorem ipsum dolor sore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            answers: [
-                'consectetur adipiiusmod tempor',
-                'consectetur adipiiusmod tempor',
-                'adipiscing elit, sed do eiusmod tempor',
-                'fasdf adipiscing elit, sed do eiusmod tempor',
-            ],
-            correctAnswer: 1,
-            hint: '',
-            explanation:
-                'm dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et do',
-        },
-        {
-            type: 'single-choose',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            answers: [
-                'piscing elit, sed do eiusmod tempor',
-                'consectetur adipiiusmod tempor',
-                'adipiscing elit, sed do eiusmod tempor',
-                'fasdf adipiscing elit, sed do eiusmod tempor',
-            ],
-            correctAnswer: 0,
-            hint: '',
-            explanation:
-                'm dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et do',
-        },
-        {
-            type: 'single-choose',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            answers: [
-                'consectetur adipiiusmod tempor',
-                'adipiscing elit, sed do eiusmod tempor',
-                'fasdf adipiscing elit, sed do eiusmod tempor',
-            ],
-            correctAnswer: 1,
-            hint: '',
-            explanation:
-                'm dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et do',
-        },
-        {
-            type: 'single-choose',
-            content:
-                'Lorem ipsum dolor sore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            answers: [
-                'consectetur adipiiusmod tempor',
-                'consectetur adipiiusmod tempor',
-                'adipiscing elit, sed do eiusmod tempor',
-                'fasdf adipiscing elit, sed do eiusmod tempor',
-            ],
-            correctAnswer: 1,
-            hint: '',
-            explanation:
-                'm dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et do',
+                'Callback được đưa vào vùng Web API, chờ một khoảng thời gian và được đưa vào Queue. Sau đó Event Loop sẽ đẩy lần lượt các hàm đang đợi trong Queue vào Call Stack nếu Stack đang trống',
         },
     ],
 };
